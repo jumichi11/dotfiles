c43ce093ec0fa0fe55a6ee51d098f8d98f37f202
jumichi11 <teion_ga_hara_ni_hibku_ze@yahoo.co.jp>
2015-08-02 19:17:33 +0900

    15-08-02更新2

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/sh
 
+ln -sf ~/dotfiles/.clang-format ~/.clang-format
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/_vimrc ~/_vimrc
 ln -sf ~/dotfiles/asciidoc_templete.asciidoc ~/.asciidoc_templete
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
